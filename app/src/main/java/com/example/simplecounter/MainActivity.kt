1452016420a270a84c8fbbfc26dd755c1a27081d
unknown <lechugoshops@gmail.com>
2026-01-29 18:56:50 -0500

    final

--- app/src/main/java/com/example/simplecounter/MainActivity.kt
+++ app/src/main/java/com/example/simplecounter/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.simplecounter
 
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
@@ -22,9 +23,25 @@ class MainActivity : AppCompatActivity() {
 
         val textView = findViewById<TextView>(R.id.textView)
         val button = findViewById<Button>(R.id.button)
+        val button2 = findViewById<Button>(R.id.button2)
+
         button.setOnClickListener {
             //Toast.makeText(this, "Clicked Button!", Toast.LENGTH_SHORT).show()
             counter++
+            textView.text = counter.toString()
+            if (counter >= 10) {
+                button2.visibility = View.VISIBLE
+            }
+        }
+
+        button2.visibility = View.INVISIBLE
+        if (counter >= 10) {
+            button2.visibility = View.VISIBLE
+        }
+
+        button2.setOnClickListener {
+            //Toast.makeText(this, "Clicked Button!", Toast.LENGTH_SHORT).show()
+            counter += 2;
             textView.text = counter.toString()
         }
     }
